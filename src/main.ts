3178a6265163b1af2ce1886d640461295d6fc83f
Admin <estevo.bussi@gmail.com>
2026-08-04 23:56:36 -0300

    chore: remove arquivos soltos acidentais da raiz do repositorio

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient() // Injeta o HttpClient para falar com o Spring Boot
-  ]
-}).catch((err) => console.error(err));
